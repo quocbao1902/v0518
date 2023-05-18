563a834e4606785e87d761fd30c865acba55d030
Nguyen Quoc Bao <130636520+quocbao1902@users.noreply.github.com>
2023-05-18 15:36:30 +0900

    練習5-11

--- v0518/Form1.cs
+++ v0518/Form1.cs
@@ -6,5 +6,45 @@ namespace v0518
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button1.Left = 0;
+            button1.Left = button1.Left + 10;
+            button1.Top = button1.Top + 10;
+            button1.Text = "çsèH";
+            button1.BackColor = Color.LightSkyBlue;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Left = button2.Left - 10;
+            button2.Top = button2.Top - 10;
+            button2.Text = "ÉiÉqÅ[É_";
+            button2.BackColor = Color.LightSeaGreen;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Left = button3.Left - 10;
+            button3.Top = button3.Top + 10;
+            button3.Text = "ã„ãSîE";
+            button3.BackColor = Color.MediumPurple;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Left = button4.Left + 10;
+            button4.Top = button4.Top - 10;
+            button4.Text = "ó∑êl";
+            button4.BackColor = Color.LightYellow;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
